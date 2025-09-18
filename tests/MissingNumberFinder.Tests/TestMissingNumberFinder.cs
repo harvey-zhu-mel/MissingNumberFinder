e33a5454bd1b492a514cef10ed18245e1521d4ac
harvey-zhu-mel <67559296+harvey-zhu-mel@users.noreply.github.com>
2025-09-18 22:17:38 +1000

    tests: add test project and unit tests

---
+++ tests/MissingNumberFinder.Tests/TestMissingNumberFinder.cs
@@ -0,0 +1,53 @@
+﻿using MissingNumberFinder.ConsoleApp;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MissingNumberFinder.Tests;
+
+[TestClass]
+public sealed class MissingNumberFinderTest
+{
+    private IMissingNumberFinder _finder = null!;
+
+    [TestInitialize]
+    public void Setup()
+    {
+        // Use the ArithmeticSeriesMissingNumberFinder implementation
+        _finder = new ArithmeticSeriesMissingNumberFinder();
+    }
+
+    [TestMethod]
+    public void TestMissingNumber_SmallArray()
+    {
+        int[] numbers = { 3, 0, 1 };
+        int expected = 2;
+
+        int actual = _finder.FindMissingNumber(numbers);
+
+        Assert.AreEqual(expected, actual, "The missing number should be 2.");
+    }
+
+    [TestMethod]
+    public void TestMissingNumber_LargeArray()
+    {
+        int[] numbers = { 9, 6, 4, 2, 3, 5, 7, 0, 1 };
+        int expected = 8;
+
+        int actual = _finder.FindMissingNumber(numbers);
+
+        Assert.AreEqual(expected, actual, "The missing number should be 8.");
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(System.ArgumentNullException))]
+    public void TestMissingNumber_NullArray()
+    {
+        _finder.FindMissingNumber(null!);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(System.ArgumentException))]
+    public void TestMissingNumber_EmptyArray()
+    {
+        _finder.FindMissingNumber(new int[0]);
+    }
+}
